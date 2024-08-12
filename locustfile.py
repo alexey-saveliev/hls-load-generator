c58f97ed1cd3d061e9997b437104dcfc2ca345ff
Alexey A. Saveliev <alexey.a.saveliev@gmail.com>
2024-08-12 16:40:39 +0000

    Remove stream quality random selection

--- locustfile.py
+++ locustfile.py
@@ -3,11 +3,8 @@ import requests
 import random
 session = requests.Session()
 #Put your stream master file address here
-master_url =  "https://example.com/TEST/STREAM/master.m3u8"
-session_url = master_url.rsplit('/', 1)[0]
-master_response = session.get(master_url)
-master_content = master_response.text
-quality_list = [x for x in master_content.split('\n') if 'index.m3u8' in x]
+stream_url =  "https://<hls-proxy>/video/3/index.m3u8"
+session_url = stream_url.rsplit('/', 1)[0]
 
 class HLSUser(HttpUser):
     wait_time = between(1, 2)
@@ -15,12 +12,11 @@ class HLSUser(HttpUser):
     @task
     def get_variant_playlist(self):
         try:
-            rand_quality = random.choice(quality_list)
-            playlist_url = session_url + '/' + rand_quality
+            playlist_url = stream_url
             playlist_response = self.session.get(playlist_url)
             playlist = playlist_response.text
-            ts = [x for x in playlist.split('\n') if x.endswith('.ts')]
-            ts_url = session_url + '/' + rand_quality.rsplit('/', 1)[0] + '/' + ts[-1]
+            ts = [x for x in playlist.split('\n') if x.endswith('.ts')]            
+            ts_url = session_url + '/' + ts[-1]
             self.client.get(ts_url)
         except Exception as e:
             print(f"Error occurred: {e}")
